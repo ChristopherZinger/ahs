ebc141ee2096c4ff24ea836d1b900b469e0cf539
ChristopherZin <krzysztof.zinger@gmail.com>
2022-02-06 20:17:29 +0100

    send verification email on signup (#2)
    * wip
    
    * send email verify email

--- src/routes/app/index.svelte
+++ src/routes/app/index.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
+	import { authEmailAndPassword } from '$src/firebase/auth/emailAndPassword';
+
 	import { userStore } from '$src/stores/userStore';
+
+	function verifyEmail() {
+		if ($userStore) {
+			authEmailAndPassword.setEmailVerification($userStore);
+		}
+	}
 </script>
 
-<h5>/app : {$userStore?.email}</h5>
+<h5>/app :</h5>
+{#if $userStore}
+	<p>
+		{$userStore.email}
+	</p>
+	<p>has verified email: {$userStore.emailVerified}</p>
+	<button on:click={verifyEmail}>verify your email</button>
+{/if}
