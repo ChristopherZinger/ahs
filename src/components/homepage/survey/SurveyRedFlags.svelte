e4fd24cfc7341e68108d067df09d5aa942096548
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-02-25 23:45:25 +0100

    abstract checkbox

--- src/components/homepage/survey/SurveyRedFlags.svelte
+++ src/components/homepage/survey/SurveyRedFlags.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Checkbox from '$src/components/form/Checkbox.svelte';
 	import {
 		RedFlagsCategories,
 		RedFlagsDisplayNames,
@@ -17,26 +18,17 @@
 </script>
 
 <div class="survey-red-flags">
-	<h5 style="margin-bottom: 0.5em">Red Flags</h5>
-	<p style="max-width: 600px">
+	<h2 class="mb-10">Red Flags</h2>
+	<p class="max-w-2xl">
 		Sign up all the bullshit that is going on in {$surveyModeStore.office.name || '___'}
 	</p>
 	{#each Object.keys(RedFlagsDisplayNames) as category, i}
 		{#if category in RedFlagsDisplayNames}
 			<div class="survey-red-flags__category">
 				<h6 class="survey-red-flags__category-title">{i + 1}. {category}:</h6>
-				<div class="survey-red-flags__checkboxes">
+				<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-x-8 gap-y-5">
 					{#each getCategory(category) as [key, text]}
-						<div class="survey-red-flags__checkbox">
-							<label for="red-flag--{key}" class:checked={redFlags[category][key]}>{text}</label>
-							<input
-								type="checkbox"
-								id="red-flag--{key}"
-								name="name"
-								bind:checked={redFlags[category][key]}
-								hidden
-							/>
-						</div>
+						<Checkbox {text} checked={redFlags[category][key]} {key} />
 					{/each}
 				</div>
 			</div>
@@ -48,69 +40,5 @@
 	.survey-red-flags__category-title {
 		margin-top: var(--spacing-xxl);
 		margin-bottom: var(--spacing-l);
-	}
-	.survey-red-flags__checkboxes {
-		display: flex;
-		flex-wrap: wrap;
-		column-gap: var(--spacing);
-		row-gap: var(--spacing-xl);
-	}
-
-	.survey-red-flags__checkbox {
-		width: 100%;
-		box-sizing: border-box;
-		position: relative;
-	}
-
-	label {
-		cursor: pointer;
-		margin-left: 50px;
-	}
-
-	label::before {
-		content: '';
-		left: 0;
-		position: absolute;
-		display: inline-block;
-		width: 35px;
-		height: 19px;
-		border: 1px solid var(--color-gray);
-		background-color: var(--color-white);
-		border-radius: 10px;
-		margin-right: 10px;
-		transition-duration: 0.3s;
-	}
-
-	label::after {
-		content: '';
-		position: absolute;
-		top: 3px;
-		left: 3px;
-		display: inline-block;
-		width: 15px;
-		height: 15px;
-		background-color: var(--color-gray-dark);
-		border-radius: 50%;
-		transition-duration: 0.3s;
-	}
-
-	label.checked::before {
-		background-color: var(--color-orange);
-		border-color: var(--color-orange);
-	}
-
-	label.checked::after {
-		left: 18px;
-		background-color: var(--color-white);
-	}
-
-	label.checked {
-		text-decoration: underline;
-	}
-
-	@media only screen and (min-width: 786px) {
-		.survey-red-flags__checkbox {
-			width: calc((100% - (2 * var(--spacing-l))) / 3);
-		}
 	}
 </style>
