d8b742a8c7e3cf93e31e0d65fbf37b9ece6c871e
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-02-26 11:12:06 +0100

    correct llink style

--- src/components/buttons/Link.svelte
+++ src/components/buttons/Link.svelte
@@ -2,9 +2,10 @@
 	export let href = '#';
 	export let size: 'xs' | 's' | 'm' | 'l' = 's';
 	export let style: string = '';
+	export let underline = true;
 </script>
 
-<a {href} class={`${size}`} {style}
+<a {href} class={`${size} ${underline} flex gap-x-2 items-center`} {style}
 	><slot />
 	{#if $$slots.icon}
 		<span class="icon"><slot name="icon" /></span>
@@ -15,6 +16,10 @@
 	a,
 	a:visited {
 		color: var(--color-black);
+	}
+
+	a.underline {
+		text-decoration: underline;
 	}
 
 	a.xs {
