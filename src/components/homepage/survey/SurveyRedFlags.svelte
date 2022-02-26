c9e5395d83dee130a32ba676a1e29891ab90de18
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-02-26 17:59:21 +0100

    optimize SurveyRedFlags component

--- src/components/homepage/survey/SurveyRedFlags.svelte
+++ src/components/homepage/survey/SurveyRedFlags.svelte
@@ -1,33 +1,18 @@
 <script lang="ts">
 	import Checkbox from '$src/components/form/Checkbox.svelte';
-	import {
-		RedFlagsCategories,
-		RedFlagsDisplayNames,
-		RedFlagsValues
-	} from '$src/firebase/survey/shema';
-	// import { surveyModeStore } from '$src/stores/surveyModeStore';
+	import { RedFlagsDisplayNames, RedFlagsValues } from '$src/firebase/survey/shema';
 
 	export let redFlags: RedFlagsValues;
-
-	const getCategory = (category: string): [string, string][] => {
-		if (category in RedFlagsDisplayNames) {
-			return Object.entries(RedFlagsDisplayNames[category as RedFlagsCategories]);
-		}
-		return [];
-	};
 </script>
 
 <div class="survey-red-flags">
 	<h4 class="mb-10">Red Flags</h4>
-	<!-- <p class="max-w-2xl">
-		Sign up all the bullshit that is going on in {$surveyModeStore.office.name || '___'}
-	</p> -->
-	{#each Object.keys(RedFlagsDisplayNames) as category, i}
+	{#each Object.entries(RedFlagsDisplayNames) as [category, flags], i}
 		{#if category in RedFlagsDisplayNames}
-			<div class="survey-red-flags__category">
-				<h6 class="survey-red-flags__category-title">{i + 1}. {category}:</h6>
+			<div>
+				<h6 class="mb-8 mt-16">{i + 1}. {category}:</h6>
 				<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-x-8 gap-y-5">
-					{#each getCategory(category) as [key, text]}
+					{#each Object.entries(flags) as [key, text]}
 						<Checkbox {text} checked={redFlags[category][key]} {key} />
 					{/each}
 				</div>
@@ -35,10 +20,3 @@
 		{/if}
 	{/each}
 </div>
-
-<style>
-	.survey-red-flags__category-title {
-		margin-top: var(--spacing-xxl);
-		margin-bottom: var(--spacing-l);
-	}
-</style>
