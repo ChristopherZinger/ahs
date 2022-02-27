cc739f57b2048099fb49d318a40ed1334805a3a1
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-02-27 17:33:22 +0100

    init firebase in functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,11 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
+const firebase = admin.initializeApp();
+const firestore = firebase.firestore();
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((_, response) => {
 	functions.logger.info('Hello logs!', { structuredData: true });
