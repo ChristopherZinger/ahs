e51ef2532d422e5415b8172fd1ae853f9b49f5d3
ChristopherZinger <krzysztof.zinger@gmail.com>
2022-02-27 17:31:13 +0100

    connect app to emulators

--- src/firebase/appFirebase.ts
+++ src/firebase/appFirebase.ts
@@ -4,7 +4,7 @@ import type { FirebaseApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import type { Auth } from 'firebase/auth';
 import config from './config';
-import { getFirestore } from 'firebase/firestore';
+import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
 import type { Firestore } from 'firebase/firestore';
 import { getFunctions, connectFunctionsEmulator } from 'firebase/functions';
 
@@ -19,12 +19,18 @@ class AppFirebase {
 		this.firestore = getFirestore(this.firebase);
 
 		this.initFirebaseFunctionsEmulator();
+		this.initFirebaseFirestoreEmulator();
 	}
 
 	private initFirebaseFunctionsEmulator() {
-		console.log('start emulators');
+		console.log('Starting firebase functions emulator.');
 		const functions = getFunctions(this.firebase);
 		connectFunctionsEmulator(functions, 'localhost', 5001);
+	}
+
+	private initFirebaseFirestoreEmulator() {
+		console.log('Starting firebase firestore emulator.');
+		connectFirestoreEmulator(this.firestore, 'localhost', 8080);
 	}
 
 	public getFirebase() {
